03101b4475a669f4598c0c9cfbe1ae5a7b3f4c33
edu <eduarroyo@gmail.com>
2013-02-20 20:08:09 +0100

    Vistas menú (v/h) y traducción

--- Asteroides/gen/com/example/asteroides/R.java
+++ Asteroides/gen/com/example/asteroides/R.java
@@ -14,7 +14,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int LinearLayout1=0x7f070000;
+        public static final int button1=0x7f070001;
+        public static final int button2=0x7f070002;
+        public static final int button3=0x7f070003;
+        public static final int button4=0x7f070004;
+        public static final int menu_settings=0x7f070007;
+        public static final int tableRow1=0x7f070005;
+        public static final int tableRow2=0x7f070006;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -23,9 +30,14 @@ public final class R {
         public static final int main=0x7f060000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int Acercade=0x7f040002;
+        public static final int Arrancar=0x7f040000;
+        public static final int Configurar=0x7f040001;
+        public static final int Salir=0x7f040003;
+        public static final int app_name=0x7f040006;
+        public static final int hello=0x7f040005;
+        public static final int menu_settings=0x7f040007;
+        public static final int tituloAplicacion=0x7f040004;
     }
     public static final class style {
         /** 
