bc930dfb0af40d7f8ae1bd08412f99e5a17ed30a
edu <eduarroyo@gmail.com>
2013-02-20 21:23:07 +0100

    Ejercicio terminado

--- MasVistas/src/com/example/masvistas/MasVistasActivity.java
+++ MasVistas/src/com/example/masvistas/MasVistasActivity.java
@@ -4,14 +4,22 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MasVistasActivity extends Activity {
 
+	private EditText entrada;
+	private TextView salida;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		entrada = (EditText) findViewById(R.id.entrada);
+		salida = (TextView)  findViewById(R.id.salida);
 	}
 
 	@Override
@@ -21,8 +29,24 @@ public class MasVistasActivity extends Activity {
 		return true;
 	} 
 	
+//  Método antiguo
+//	public void sePulsa(View view) {
+//        Toast.makeText(this, "Pulsado", Toast.LENGTH_SHORT).show();
+//  }
+	
 	public void sePulsa(View view) {
-        Toast.makeText(this, "Pulsado", Toast.LENGTH_SHORT).show();
-    }
-
+		try
+		{
+			float n = Float.parseFloat(entrada.getText().toString());
+			salida.setText(String.valueOf(n * 2.0f));
+		}
+		catch(NumberFormatException e)
+		{
+			Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+		}
+	}
+	
+	public void sePulsa0(View view){
+		entrada.setText(entrada.getText() + (String) view.getTag());
+	}
 }
